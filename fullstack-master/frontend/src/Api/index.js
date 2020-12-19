f19345b5db991e51ab18368d949da4fe90140327
dandolal <dan.dolgov@gmail.com>
2020-12-19 15:02:03 +0300

    initial commit

--- fullstack-master/frontend/src/Api/index.js
+++ fullstack-master/frontend/src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: (process.env.REACT_APP_BASE_URL || 'http://localhost:8080') + '/api',
+    baseURL: ('http://35.226.66.252:8001' || 'http://localhost:8080') + '/api',
     responseType: 'json'
 })
